c1effd5bb239180f8846691dc8167524c594160b
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-05-03 16:50:23 +0300

    :zap: added material ui and styled components

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      SLACK
+      <Router>
+      <>
+        <Switch>
+          <Route path="/" exact>
+            Slack
+          </Route>
+        </Switch>
+      </>
+    </Router>
     </div>
   );
 }
---
+++ slack/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SendIcon from '@material-ui/icons/Send';
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
