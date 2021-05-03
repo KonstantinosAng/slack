d5cb4072f8432fc61f6dd9f776345f343b2e170d
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-05-03 19:57:47 +0300

    :zap: progress

--- slack/src/components/Sidebar.js
+++ slack/src/components/Sidebar.js
@@ -12,6 +12,7 @@ import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
   return (
@@ -34,6 +35,11 @@ function Sidebar() {
       <SidebarOption Icon={AppsIcon} title="Apps" />
       <SidebarOption Icon={FileCopyIcon} title="File browser" />
       <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+      <SidebarOption title="World"/>
     </SidebarContainer>      
   )
 }
@@ -47,6 +53,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
--- slack/src/components/SidebarOption.js
+++ slack/src/components/SidebarOption.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function SidebarOption({Icon, title}) {
+function SidebarOption({Icon, title, addChannelOption}) {
+
+  const addChannel = () => {
+
+  }
+
+  const selectChannel = () => {
+
+  }
+
   return (
-    <SidebarOptionContainer>
-      <Icon />
-      <h3> {title} </h3>
+    <SidebarOptionContainer onClick={addChannelOption ? addChannel : selectChannel}>
+      {Icon && <Icon fontSize='small' style={{ padding: 10 }} />}
+      {Icon 
+      ? 
+      ( <h3> {title} </h3> )
+      : 
+      (
+      <SidebarOptionChannel>
+        <span> # </span> { title }
+      </SidebarOptionChannel>
+      )}
     </SidebarOptionContainer>
   )
 }
@@ -14,5 +31,26 @@ export default SidebarOption;
 
 const SidebarOptionContainer = styled.div`
   display: flex;
-  flex: 1;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+
+  :hover {
+    cursor: pointer;
+    opacity: .9;
+    background-color: #340e36;
+  }
+  
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
 `;
+
+const SidebarOptionChannel = styled.div`
+  cursor: pointer;
+`;
+
