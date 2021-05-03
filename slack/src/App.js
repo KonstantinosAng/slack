8d5d66038a036c588882b4f19f349ec957609b01
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-05-03 19:02:10 +0300

    :zap: build header

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <>
         <Switch>
           <Route path="/" exact>
-            Slack
+            <Header />
           </Route>
         </Switch>
       </>
--- slack/src/components/Header.js
+++ slack/src/components/Header.js
@@ -1,12 +1,92 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 import SendIcon from '@material-ui/icons/Send';
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
+
   return (
-    <div>
-      
-    </div>
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search Community" />
+      </HeaderSearch>
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
+    </HeaderContainer>
   )
 }
 
-export default Header
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  flex: .3;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: .4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div `
+  display: flex;
+  flex: .3;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: .8;
+  }
+
+`;
